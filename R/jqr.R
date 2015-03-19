cada4864f75480cc6f63a6f72b9af0294aa869c2
Scott Chamberlain <myrmecocystus@gmail.com>
2015-03-19 12:13:53 -0700

    rbildignore readme.rmd, added readme.rmd, exporting jq_ for low level interface

--- R/jqr.R
+++ R/jqr.R
@@ -1,8 +1,22 @@
-#' jqr execute function
+#' Execute a query with jq
 #'
+#' \code{jq} is meant to work with the high level interface in this package. \code{jq_}
+#' provides access to the low level interface in which you can use jq query strings just
+#' as you would on the command line. \code{show} prints the query resulting from \code{jq}
+#' all in one character string just as you would execute it on the command line.
+#'
+#' @param .data (list) input, using higher level interface
+#' @param data (character) JSON data
+#' @param query (character) A query string
 #' @export
 jq <- function(.data) {
   jqr(.data$data, make_query(.data))
+}
+
+#' @export
+#' @rdname jq
+jq_ <- function(data, query) {
+  jqr(data, query)
 }
 
 #' @export
