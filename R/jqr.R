81e27fcf914d517b204144f41d7a13b5a5670a93
Scott Chamberlain <myrmecocystus@gmail.com>
2015-03-18 23:03:07 -0700

    added pipe to export, importing magrittr

--- R/jqr.R
+++ R/jqr.R
@@ -1,8 +1,12 @@
-# jqr execute function
+#' jqr execute function
+#'
+#' @export
 jq <- function(.data) {
   jqr(.data$data, make_query(.data))
 }
 
+#' @export
+#' @rdname jq
 show <- function(.data) {
   make_query(.data)
 }
