fb8fc7cca9482f4ec13167e55a9b15318207459e
Rich FitzJohn <rich.fitzjohn@gmail.com>
2015-08-17 15:17:44 +0100

    Upgrade from jq 1.4 to 1.5
    Unfortunately this is not that seamless, and we might be ahead of the game here
    as the jq source tarball does not include the autoconf-generated files (configure,
    etc).  But this does compile and pass all tests, so that's a good sign.
    
    Fixes #15
    Fixes #9

--- bootstrap.R
+++ bootstrap.R
@@ -5,43 +5,34 @@
 ## this will need to be rerun, but from now should be run only by
 ## mantainers, rather than by users.
 
-url <- "http://stedolan.github.io/jq/download/source/jq-1.4.tar.gz"
-if (!file.exists("jq-1.4.tar.gz")) {
-  download.file(url, "jq-1.4.tar.gz")
+url <- "https://github.com/stedolan/jq/archive/jq-1.5.tar.gz"
+if (!file.exists("jq-1.5.tar.gz")) {
+  downloader::download(url, "jq-1.5.tar.gz")
 }
 if (!file.exists(Sys.getenv("TAR"))) {
   Sys.setenv(TAR="/usr/bin/tar")
 }
-if (file.exists("src/jq-1.4")) {
-  unlink("src/jq-1.4", recursive=TRUE)
+if (file.exists("src/jq-1.5")) {
+  unlink("src/jq-1.5", recursive=TRUE)
 }
 
-untar("jq-1.4.tar.gz", compressed=TRUE, exdir="src")
-
-## This file is necessary, but it causes a WARNING.  I don't see any
-## reference to this file on CRAN (lt~obsolete.m4 user:cran), so it
-## seems worth dealing with.  I'm just renaming it for now and
-## updating references.
-files <- c("aclocal.m4", "Makefile.in")
-fix <- function(str) {
-  sub("lt~obsolete.m4", "lt__obsolete.m4", str, fixed=TRUE)
-}
-for (f in file.path("src/jq-1.4", files)) {
-  str <- fix(readLines(f))
-  writeLines(str, f)
-}
-ok <- file.rename("src/jq-1.4/config/m4/lt~obsolete.m4",
-                  "src/jq-1.4/config/m4/lt__obsolete.m4")
+untar("jq-1.5.tar.gz", compressed=TRUE, exdir="src")
+## Heh - the download goes into the wrong directory :(
+file.rename("src/jq-jq-1.5", "src/jq-1.5")
 
 header <-
   c("The following license applies to code from the jq library which will",
     "be linked into the installed package")
-writeLines(c(header, "", readLines("src/jq-1.4/COPYING")),
+writeLines(c(header, "", readLines("src/jq-1.5/COPYING")),
            "inst/COPYING.jq")
 
 ## Drop some of the bits:
-unlink("src/jq-1.4/docs", recursive=TRUE)
-unlink("src/jq-1.4/tests", recursive=TRUE)
+unlink("src/jq-1.5/docs", recursive=TRUE)
+unlink("src/jq-1.5/tests", recursive=TRUE)
 
 ## Rewite the version script
-writeLines(c("#!/bin/sh", "echo 1.4"), "src/jq-1.4/scripts/version")
+writeLines(c("#!/bin/sh", "echo 1.5"), "src/jq-1.5/scripts/version")
+
+owd <- setwd("src/jq-1.5")
+on.exit(setwd(owd))
+system2("autoreconf", "-i")
